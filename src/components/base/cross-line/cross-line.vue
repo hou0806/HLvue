8aa4d974792d09c24d24ec4424fa1842decc7767
hou0806 <850672273@qq.com>
2019-07-15 18:59:34 +0800

    提交文件

---
+++ src/components/base/cross-line/cross-line.vue
@@ -0,0 +1,34 @@
+<!-- 横线分隔条 组件 -->
+
+<template>
+  <div class="cross-line"></div>
+</template>
+
+<script>
+export default {
+  components: {},
+  data () {
+    return {}
+  },
+  props: {},
+  watch: {},
+  methods: {},
+  filters: {},
+  computed: {},
+  created () {},
+  mounted () {},
+  destroyed () {}
+}
+</script>
+
+<style lang="scss" scoped>
+@import '~@/assets/scss/const.scss';
+@import '~@/assets/scss/mixin.scss';
+
+.cross-line {
+  width: 100%;
+  height: 16px;
+  background-color: #ebebeb;
+  
+}
+</style>
